041b3bc9f4a4a2533220eb5a286e435c5c0268b0
MrHouse111 <dusanbalesevic7@gmail.com>
2025-05-13 17:05:46 +0200

    Configure Next.js for static export and add GitHub Actions workflow

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: 'loose'
+  output: 'export', // Ovo je ključno za statički izvoz
+  // Ako tvoj sajt NEĆE biti na root domeni (npr. https://tvojeime.github.io/NartStudioNata/)
+  // ONDA OBAVEZNO DODAJ basePath i assetPrefix.
+  // Ako je ime tvog repozitorijuma 'NartStudioNata', onda će basePath biti '/NartStudioNata'
+  basePath: '/NartStudioNata',
+  assetPrefix: '/NartStudioNata/', // Obavezno sa kosom crtom na kraju
+  images: {
+    unoptimized: true, // Potrebno za next/image sa statičkim izvozom na GitHub Pages
   },
-  webpack: (config) => {
-    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
-    return config;
-  },
+  trailingSlash: true, // Opciono, ali često korisno za GitHub Pages da ispravno rutira
 };
 
 module.exports = nextConfig;
